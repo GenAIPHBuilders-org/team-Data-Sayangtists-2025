c2d3f8697223df80ab081b53b71f2cb57c0f095d
FreyaKassie16 <shaunmarvin.acenas@gmail.com>
2025-04-30 03:50:46 +0800

    Change text color of New Entry button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/global.css' // Add this line
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
